1ab155cf63d47ac45a9c2edc920b38372dc1bd75
bettaibi <bettaibinidhal00@gmail.com>
2022-02-16 19:19:12 +0100

    added by nidhal

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div>
-      Chatup application is running
+      <h6>
+        Chatup application is running
+      </h6>
     </div>
   );
 }
